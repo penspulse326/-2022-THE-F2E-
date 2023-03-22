4fe9fbcf5870c2fa28c45fdde367c02998b9bdc1
SHIN <akiyachiumi@gmail.com>
2023-03-22 18:37:20 +0800

    section2 start

--- week1/src/js/main.js
+++ week1/src/js/main.js
@@ -2,35 +2,55 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { TextPlugin } from "gsap/dist/TextPlugin";
 
-window.addEventListener("load", () => {
-  setTimeout(() => {
-    document.getElementById("root").style.display = "block";
-    document.querySelector(".loading").style.display = "none";
-  }, 10);
-});
-
-const optionItem = document.querySelector(".option-items");
-const optionBox = document.querySelector(".jumbo-option");
-
-optionItem.addEventListener("mouseover", (e) => {
-  const login = document.querySelector("#option-login");
-  const signup = document.querySelector("#option-signup");
-  if (e.target === login) {
-    optionBox.style.alignItems = "flex-start";
-    login.style.color = "#B82A06";
-    signup.style.color = "white";
-  }
-  if (e.target === signup) {
-    optionBox.style.alignItems = "flex-end";
-    login.style.color = "white";
-    signup.style.color = "#B82A06";
-  }
-});
-
 gsap.registerPlugin(ScrollTrigger, TextPlugin);
 
+// loading
 gsap.to(".loadText", {
   text: "Loading...",
   duration: 5,
   repeat: -1,
 });
+
+// loading complete
+let blinkTween = null;
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    document.getElementById("root").style.display = "block";
+    document.querySelector(".loading").style.display = "none";
+    blinkTween = Blink(login);
+  }, 10);
+});
+
+// options button event
+const optionItem = document.querySelector(".option-items");
+const optionBox = document.querySelector(".jumbo-option");
+const login = document.querySelector("#option-login");
+const signup = document.querySelector("#option-signup");
+
+optionItem.addEventListener("mouseover", (e) => {
+  if (e.target === login || signup) {
+    const another = e.target === login ? signup : login;
+    blinkTween.kill();
+    blinkTween = Blink(e.target);
+    optionBox.style.alignItems = e.target === login ? "flex-start" : "flex-end";
+    e.target.style.color = "#B82A06";
+    another.style.color = "white";
+    another.style.opacity = 1;
+  }
+});
+
+function Blink(element) {
+  return gsap.fromTo(
+    element,
+    {
+      autoAlpha: 1.0,
+    },
+    {
+      autoAlpha: 0,
+      duration: 0.5,
+      repeat: -1,
+      yoyo: true,
+    }
+  );
+}
